8a90d4a522ba24523b6d6910727eec6bbbb89d52
Your Name <you@example.com>
2023-05-27 13:01:21 +0530

    Final

--- components/Form.jsx
+++ components/Form.jsx
@@ -5,7 +5,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">{type} Post</span>
+        <span className="blue_gradient">{type} Memory</span>
       </h1>
       <form
         onSubmit={handleSubmit}
